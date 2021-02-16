67a3980c934ea1fc7d1a0266ef8fa18bc741e0da
OnlineTeachingNow <startonlineteachingnow@gmail.com>
2021-02-16 11:36:49 +0100

    Adding preexisting Number Wizard Project to GitHub.

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int maxNumber;
+    [SerializeField] int minNumber;
+    [SerializeField] public int maxGuesses;
+    [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] TextMeshProUGUI guessNumber;
+    int guess; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        guessNumber.text = maxGuesses.ToString();
+    }
+    public void OnPressHigher()
+    {
+        if (maxGuesses == 0)
+        {
+            SceneManager.LoadScene(7);
+        }
+        else
+        {
+            minNumber = guess + 1;
+            NextGuess();
+            maxGuesses--;
+            guessNumber.text = maxGuesses.ToString();
+        }
+    }
+
+    public void OnPressLower()
+    {
+        if (maxGuesses == 0)
+        {
+            SceneManager.LoadScene(7);
+        }
+        else
+        {
+            maxNumber = guess - 1;
+            NextGuess();
+            maxGuesses--;
+            guessNumber.text = maxGuesses.ToString();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(minNumber, maxNumber);
+        guessText.text = guess.ToString();
+    }
+}
